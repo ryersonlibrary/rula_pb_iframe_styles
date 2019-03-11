f6449a981c58a65560c8bb134de841dbf8c3ff0e
Patrick Fung <patrick@makestuffdostuff.com>
2019-03-11 12:33:45 -0400

    more css tweaks

--- rula-pb-iframe-styles.php
+++ rula-pb-iframe-styles.php
@@ -6,7 +6,7 @@
  * Author URI: https://github.com/ryersonlibrary
  * Description: Hides the header and footer when Pressbooks is loaded within an iframe.
  * GitHub Plugin URI: https://github.com/ryersonlibrary/rula_pb_iframe_styles
- * Version: 0.3.8
+ * Version: 0.3.9
  */
 
 // Include our custom settings page for the plugin
